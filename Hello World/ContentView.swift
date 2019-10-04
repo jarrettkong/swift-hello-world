75f7e6afb42afea296d4296029fe003500575e69
Jarrett Kong <jarrettkong@me.com>
2019-10-04 11:09:52 -0600

    Initial Commit

---
+++ Hello World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by Jarrett Kong on 10/4/19.
+//  Copyright © 2019 Jarrett Kong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
